052a7299cab4ba95f2b2ac6d46a3e4069f7e727c
Tom Gregor <tgregor@ivsoftware.com>
2023-02-11 17:55:52 -0700

    Add project files.

---
+++ IVSoftware.Portable.WatchdogTimer/WatchdogTimer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace IVSoftware.Portable
+{
+    public class WatchdogTimer
+    {
+        int _wdtCount = 0;
+        public TimeSpan Interval { get; set; } = TimeSpan.FromSeconds(1);
+        public void StartOrRestart(Action action = null)
+        {
+            Running = true;
+            _wdtCount++;
+            var capturedCount = _wdtCount;
+            Task
+                .Delay(Interval)
+                .GetAwaiter()
+                .OnCompleted(() =>
+                {
+                    // If the 'captured' localCount has not changed after awaiting the Interval, 
+                    // it indicates that no new 'bones' have been thrown during that interval.        
+                    if (capturedCount.Equals(_wdtCount))
+                    {
+                        action?.Invoke();
+                        Running = false;
+                        RanToCompletion?.Invoke(this, EventArgs.Empty);
+                    }
+                });
+        }
+        public bool Running { get; private set; }
+        public event EventHandler RanToCompletion;
+    }
+}
---
+++ IVSoftware.Portable.WatchdogTimer/nuget-publish.bat
@@ -0,0 +1,1 @@
+dotnet pack --no-build --no-restore -c Release -o D:\Github\packages-iv\
