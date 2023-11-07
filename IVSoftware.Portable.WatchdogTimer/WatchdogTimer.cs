e4e5ccd26512c465787cbebc112df6b8b4f86a74
Tom Gregor <tgregor@ivsoftware.com>
2023-11-07 07:25:41 -0700

    NuGet 1.2.0-beta ORIGIN: StartOrRestart(Action initialAction, Action completeAction)

--- IVSoftware.Portable.WatchdogTimer/WatchdogTimer.cs
+++ IVSoftware.Portable.WatchdogTimer/WatchdogTimer.cs
@@ -5,15 +5,21 @@ namespace IVSoftware.Portable
 {
     public class WatchdogTimer
     {
-        int _wdtCount = 0;
-        bool _isCancelled = false;
-        public TimeSpan Interval { get; set; } = TimeSpan.FromSeconds(1);
+        int _startCount = 0;
+        int _cancelCount = 0;
+
+        /// <summary>
+        /// Restart the watchdog timer.
+        /// </summary>
+        /// <remarks>
+        /// Core method that can take a parameterized action as well as a custom EventArgs object.
+        /// </remarks>
         public void StartOrRestart(Action action, EventArgs e)
         {
             Running = true;
-            _wdtCount++;
-            var capturedCount = _wdtCount;
-            _isCancelled= false;
+            _startCount++;
+            var capturedStartCount = _startCount;
+            var capturedCancelCount = _cancelCount;
             Task
                 .Delay(Interval)
                 .GetAwaiter()
@@ -21,23 +27,73 @@ namespace IVSoftware.Portable
                 {
                     // If the 'captured' localCount has not changed after awaiting the Interval, 
                     // it indicates that no new 'bones' have been thrown during that interval.        
-                    if (capturedCount.Equals(_wdtCount) && !_isCancelled)
+                    if (capturedStartCount.Equals(_startCount) && capturedCancelCount.Equals(_cancelCount))
                     {
-                        action?.Invoke();
                         Running = false;
                         RanToCompletion?.Invoke(this, e ?? EventArgs.Empty);
+                        action?.Invoke();
                     }
                 });
         }
 
+        /// <summary>
+        /// Restart the watchdog timer.
+        /// </summary>
+        /// <remarks>
+        /// Subscribe to the RanToCompletion event to receive notification of completion.  
+        /// On completion, fire an event with an empty EventArgs object.
+        /// </remarks>
         public void StartOrRestart() => StartOrRestart(null, EventArgs.Empty);
-        public void StartOrRestart(Action action) => StartOrRestart(action, EventArgs.Empty);
+
+        /// <summary>
+        /// Restart the watchdog timer.
+        /// </summary>
+        /// <remarks>
+        /// Subscribe to the RanToCompletion event to receive notification of completion.  
+        /// On completion, fire an event using a custom parameterized EventArgs object.
+        /// </remarks>
         public void StartOrRestart(EventArgs e) => StartOrRestart(null, e);
-        
-        // By decrementing without capturing, the condition
-        // capturedCount.Equals(_wdtCount) evaluated to false.
-        public void Cancel() => _isCancelled = true;
+
+        /// <summary>
+        /// Restart the watchdog timer.
+        /// </summary>
+        /// <remarks>
+        /// Subscribe to the RanToCompletion event to receive notification of completion.  
+        /// On completion, invoke a parameterized action.
+        /// </remarks>
+        public void StartOrRestart(Action action) => StartOrRestart(action, EventArgs.Empty);
+
+        /// <summary>
+        /// Restart the watchdog timer.
+        /// </summary>
+        /// <remarks>
+        /// Invoke an initial parameterized action if not already running.
+        /// Subscribe to the RanToCompletion event to receive notification of completion.  
+        /// On completion, invoke a parameterized action.
+        /// </remarks>
+        public void StartOrRestart(Action initialAction, Action completeAction)
+        {
+            if (!Running)
+            {
+                Running = true;
+                initialAction();
+            }
+            StartOrRestart(completeAction, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Cancel all pending actions and events.
+        /// </summary>
+        public void Cancel()
+        {
+            _cancelCount++;
+            Running = false;
+            Cancelled?.Invoke(this, EventArgs.Empty);
+        }
+
+        public TimeSpan Interval { get; set; } = TimeSpan.FromSeconds(1);
         public bool Running { get; private set; }
         public event EventHandler RanToCompletion;
+        public event EventHandler Cancelled;
     }
 }
--- README.md
+++ README.md
@@ -1,12 +1,98 @@
 # Watchdog timer
 
-This timer is designed to complete after the time span set by the `Interval' property has elapsed since the most recent call to the `StartOrRestart` method.
+#### _What is it?_
 
-This functionality may be accessed in a variety of ways.
+A timer that completes _one_ time, after the TimeSpan in the `Interval` property has elapsed since the _most recent call_ to the `StartOrRestart` method, regardless of the number of restarts. Invoking the `Cancel` method negates any enqueued action or event in the queue.
+
+#### _Examples_
+
+**Display an alert after user moves the mouse**
+
+We know that we're interested in mouse move events, but obviously don't want to make a message eash time one occurs.
+
+
+**Debouncing**
+
+An impatient user might tap multiple times. A watchdog timer can ensure that an action takes place the first time, and requires a cooling off interval before the same action can happen again.
+
+***
+**Methods**
+
+    /// <summary>
+    /// Restart the watchdog timer.
+    /// </summary>
+    /// <remarks>
+    /// Core method that can take a parameterized action as well as a custom EventArgs object.
+    /// </remarks>
+    public void StartOrRestart(Action action, EventArgs e)
+    {
+        Running = true;
+        _wdtCount++;
+        var capturedCount = _wdtCount;
+        _isCancelled= false;
+        Task
+            .Delay(Interval)
+            .GetAwaiter()
+            .OnCompleted(() =>
+            {
+                // If the 'captured' localCount has not changed after awaiting the Interval, 
+                // it indicates that no new 'bones' have been thrown during that interval.        
+                if (capturedCount.Equals(_wdtCount) && !_isCancelled)
+                {
+                    action?.Invoke();
+                    Running = false;
+                    RanToCompletion?.Invoke(this, e ?? EventArgs.Empty);
+                }
+            });
+    }
+
+    /// <summary>
+    /// Restart the watchdog timer.
+    /// </summary>
+    /// <remarks>
+    /// Subscribe to the RanToCompletion event to receive notification of completion.  
+    /// On completion, fire an event with an empty EventArgs object.
+    /// </remarks>
+    public void StartOrRestart() => StartOrRestart(null, EventArgs.Empty);
+
+    /// <summary>
+    /// Restart the watchdog timer.
+    /// </summary>
+    /// <remarks>
+    /// Subscribe to the RanToCompletion event to receive notification of completion.  
+    /// On completion, fire an event using a custom parameterized EventArgs object.
+    /// </remarks>
+    public void StartOrRestart(EventArgs e) => StartOrRestart(null, e);
+
+    /// <summary>
+    /// Restart the watchdog timer.
+    /// </summary>
+    /// <remarks>
+    /// Subscribe to the RanToCompletion event to receive notification of completion.  
+    /// On completion, invoke a parameterized action.
+    /// </remarks>
+    public void StartOrRestart(Action action) => StartOrRestart(action, EventArgs.Empty);
+        
+
+    public void Cancel() => _isCancelled = true;    
+
+***
+**Properties**
+
+    public TimeSpan Interval { get; set; } = TimeSpan.FromSeconds(1);
+
+    public bool Running { get; private set; }
 
 ***
 **Event**
 
-Like most timer classes, `WatchdogTimer` fires an event when the interval has run to completion.
+    public event EventHandler RanToCompletion;
 
-    WatchdogTimer wdt = new WatchdogTimer(new TImeSpan.FromSeconds(5));
+***
+
+
+**StackOverflow**
+
+[I want to call a method after some delay when an event is raised, but any subsequent events should "restart" this delay.](https://stackoverflow.com/q/75284980/5438626)
+
+
