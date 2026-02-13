a1a9ac41e2beac1deb052a14fa88d5e51bb79e10
Tom Gregor <tgregor@ivsoftware.com>
2026-02-13 15:04:48 -0700

    UTAC @ 11 FTE

--- IVSoftware.Portable.WatchdogTimer.MSTest/TestClass_ReadMeClaims.cs
+++ IVSoftware.Portable.WatchdogTimer.MSTest/TestClass_ReadMeClaims.cs
@@ -37,7 +37,7 @@ public class TestClass_ReadMeClaims
             inputText.Clear();
             foreach (var c in new[] { 'g', 'r', 'e', 'e', 'n' })
             {
-                await Task.Delay(TimeSpan.FromSeconds(0.25));
+                await Task.Delay(inputCharacterPeriod);
                 inputText.Append(c);
                 wdt.StartOrRestart();
             }
@@ -59,7 +59,12 @@ public class TestClass_ReadMeClaims
     {
         TaskCompletionSource tcsSimStarted = new(); // Will ensure that the test enters the simulation.
         Stopwatch stopwatch = new(); // Measure epoch for test.
-        TextBoxAwaitableBaseClass textBox = new TextBoxAwaitable {  Interval = TimeSpan.FromSeconds(0.5) };
+        TimeSpan
+            inputSettleInterval = TimeSpan.FromSeconds(0.5),
+            inputCharacterPeriod = TimeSpan.FromSeconds(0.25);
+
+        var textBox = new TextBoxAwaitableBaseClass {  Interval = inputSettleInterval };
+
         textBox.EpochInitialized += (sender, e) =>
         {
             stopwatch.Start();
@@ -82,8 +87,8 @@ public class TestClass_ReadMeClaims
                 while (i < inputText.Length - 1)
                 {
                     sb.Append(inputText[i++]);
-                    await Task.Delay(TimeSpan.FromSeconds(0.25));
                     textBox.Text = sb.ToString();
+                    await Task.Delay(inputCharacterPeriod);
                 }
                 sb.Append(inputText[i]);
                 textBox.Text = sb.ToString();
@@ -94,7 +99,73 @@ public class TestClass_ReadMeClaims
             await tcsSimStarted.Task;
             await textBox; // Await deterministic epoch settlement.
             stopwatch.Stop();
-            tcsSimStarted = new(); // Ready for more cases.
+
+            var expected = textBox.EpochTimeSpanExpected("green", inputCharacterPeriod);
+            Assert.IsTrue(TimeSpan.FromSeconds(stopwatch.Elapsed.TotalSeconds) >= expected, "Expecting a minimum settle time.");
+            Assert.IsTrue(TimeSpan.FromSeconds(stopwatch.Elapsed.TotalSeconds) <= expected + TimeSpan.FromSeconds(0.5), "Expecting a maximum settle time window.");
+
+            tcsSimStarted = new(); // RESET TCS - Ready for next subtest.
+        }
+        #endregion S U B T E S T S
+    }
+
+
+
+    [TestMethod]
+    public async Task Test_OverridableAsyncHookSubclass()
+    {
+        TaskCompletionSource tcsSimStarted = new(); // Will ensure that the test enters the simulation.
+        Stopwatch stopwatch = new(); // Measure epoch for test.
+        TimeSpan
+            inputSettleInterval = TimeSpan.FromSeconds(0.5),
+            inputCharacterPeriod = TimeSpan.FromSeconds(0.25),
+            asyncFinalizationPeriod = TimeSpan.FromSeconds(1);
+
+        // Different!
+        var textBox = new TextBoxAwaitable{ Interval = inputSettleInterval };
+
+        textBox.EpochInitialized += (sender, e) =>
+        {
+            stopwatch.Start();
+            tcsSimStarted.TrySetResult();
+        };
+
+        await subtestSimulateTyping("green");
+
+        #region S U B T E S T S 
+        async Task subtestSimulateTyping(string inputText)
+        {
+            // Simulate keystrokes that would normally occur on the UI thread.
+            // - Do not await here. This is just a burst of keystrokes in the wild.
+            _ = Task.Run(async () =>
+            {
+                textBox.Clear();
+                StringBuilder sb = new();
+
+                int i = 0;
+                while (i < inputText.Length - 1)
+                {
+                    sb.Append(inputText[i++]);
+                    textBox.Text = sb.ToString();
+                    await Task.Delay(inputCharacterPeriod);
+                }
+                sb.Append(inputText[i]);
+                textBox.Text = sb.ToString();
+            });
+
+            // Without awaiting, this test would return immediately
+            // (the fire-and-forget input simulation may not execute at all).
+            await tcsSimStarted.Task;
+            await textBox; // Await deterministic epoch settlement.
+            stopwatch.Stop();
+
+            // Different
+            var expected = textBox.EpochTimeSpanExpected("green", inputCharacterPeriod) + asyncFinalizationPeriod;
+
+            Assert.IsTrue(TimeSpan.FromSeconds(stopwatch.Elapsed.TotalSeconds) >= expected, "Expecting a minimum settle time.");
+            Assert.IsTrue(TimeSpan.FromSeconds(stopwatch.Elapsed.TotalSeconds) <= expected + TimeSpan.FromSeconds(0.5), "Expecting a maximum settle time window.");
+
+            tcsSimStarted = new(); // RESET TCS - Ready for next subtest.
         }
         #endregion S U B T E S T S
     }
